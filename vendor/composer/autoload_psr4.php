37bd9ad11004f8b9b4e1850e756ab59d744fd7c9
Eduardo Moura <oeduardomoura@gmail.com>
2019-10-09 12:25:56 -0300

    DB class

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Oficina\\' => array($vendorDir . '/hcodebr/php-classes/src'),
+    'Oficina\\' => array($vendorDir . '/oficinabr/php-classes/src'),
 );
